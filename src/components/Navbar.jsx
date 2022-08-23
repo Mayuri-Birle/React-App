8c9cb45c8976a647f58456bfab51b30287f75dc3
mayuri-birle <birlemayuri181199@gmail.com>
2022-08-23 18:01:14 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Icon from '../images/icon.png'
+
+export default function Navbar() {
+  return (
+   
+      <nav>
+      <img src={Icon} className="nav--icon"/>
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+      </nav>
+    
+  );
+}
